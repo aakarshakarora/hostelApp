4922f7dccf88311e50bcbf0c7b4f0af49cab54b9
Aakarshak Arora <46300387+aakarshakarora@users.noreply.github.com>
2020-09-04 19:57:03 +0530

    firebase added

---
+++ android/app/src/main/kotlin/com/example/hostelApp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hostelApp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/dasboard/student/dashboard_student.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+
+class DashboardStudent extends StatelessWidget {
+  final titles = [
+    'Apply for OutPass',
+    'Mess Menu',
+    'Service Request',
+    'Laundry Cycles'
+  ];
+  final titleIcon = [
+    Icon(Icons.assignment),
+    Icon(Icons.local_dining),
+    Icon(Icons.settings),
+    Icon(Icons.local_laundry_service)
+  ];
+  final name = 'Name Surname';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Welcome, $name!',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                  FlatButton.icon(
+                    onPressed: () {},
+                    icon: Icon(Icons.exit_to_app),
+                    label: Text('Logout'),
+                    shape: RoundedRectangleBorder(
+                        side: BorderSide(
+                            color: Colors.black,
+                            width: 1,
+                            style: BorderStyle.solid),
+                        borderRadius: BorderRadius.circular(50)),
+                  ),
+                ],
+              ),
+              Divider(
+                thickness: 0.7,
+                color: Colors.white70,
+              ),
+              Flexible(
+                child: GridView.builder(
+                  physics: NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2),
+                  itemCount: titles.length,
+                  itemBuilder: (ctx, index) {
+                    return InkWell(
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(25),
+                          ),
+                        ),
+                        //color: Theme.of(context).primaryColor,
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              titleIcon[index],
+                              Text(
+                                titles[index],
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      onTap: () {},
+                    );
+                  },
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                child: RaisedButton(
+                  padding: const EdgeInsets.all(15),
+                  child: Text(
+                    'View OutPass Status',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  color: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18.0),
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
