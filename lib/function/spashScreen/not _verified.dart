c652ec80398e9e60771e4775f7d673c984d5a7fe
Aakarshak Arora <46300387+aakarshakarora@users.noreply.github.com>
2021-05-09 00:02:14 +0530

    minor error handling done and role check improved

--- lib/function/spashScreen/not _verified.dart
+++ lib/function/spashScreen/not _verified.dart
@@ -1,14 +1,75 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:hostel_app/login/login_SignUp%20page.dart';
 
+import '../../theme/theme.dart';
 
 class NotVerified extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Container(
-        color: Colors.white,
-        child: Center(child :Text("Please verify your email before continuing ")),
-      ),),
+      body: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Your Email ID is not Verified !! "),
+            Icon(
+              Icons.not_interested,
+              color: Colors.red,
+            )
+          ],
+        ),
+        Container(
+          padding: EdgeInsets.all(10),
+          child: Text(
+            "Click \"Get Link\" to receive Verification link on your Email ID  ",
+            textAlign: TextAlign.center,
+          ),
+        ),
+        SizedBox(height: 5),
+        Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(30)),
+              color: peach,
+            ),
+            child: MaterialButton(
+              onPressed: () {
+                sendEmailVerification();
+                _displaySnackBar(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return LoginPage();
+                    },
+                  ),
+                );
+              },
+              child: Text(
+                "Get Link",
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  color: Colors.white,
+                ),
+              ),
+            )),
+        SizedBox(height: 5),
+        Text("Verify your Email ID to Login"),
+      ]),
     );
+  }
+
+  Future<void> sendEmailVerification() async {
+    User user = FirebaseAuth.instance.currentUser;
+    user.sendEmailVerification();
+  }
+
+  _displaySnackBar(BuildContext context) {
+    final snackBar = SnackBar(
+        content: Text(
+      'Link Sent on ' + FirebaseAuth.instance.currentUser.email,
+      style: TextStyle(fontFamily: 'Poppins'),
+    ));
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 }
--- lib/function/spashScreen/splash.dart
+++ lib/function/spashScreen/splash.dart
@@ -1,166 +1,17 @@
-import 'dart:async';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:hostel_app/common/bottomBar/navigationBarHostel.dart';
-import 'package:hostel_app/common/bottomBar/navigationBarLaundry.dart';
-import 'package:hostel_app/common/bottomBar/navigationBarSecGuard.dart';
-import 'package:hostel_app/common/bottomBar/navigationBarStudent.dart';
-import 'package:hostel_app/common/bottomBar/navigationBarMess.dart';
-import 'package:hostel_app/login/login_SignUp%20page.dart';
-import 'package:hostel_app/theme/theme.dart';
 
-//Status: Minor Issues are there
-
-/*
-Persistent Login and Role Check
-*/
-
+import '../../theme/theme.dart';
 
 class SplashScreen extends StatefulWidget {
   final String text;
 
-  SplashScreen({this.text});
+  SplashScreen(this.text);
 
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-  String _userID;
-  String _switchCode;
-
-  startTimer() async {
-    var _duration = Duration(seconds: 8);
-    return Timer(_duration, navigationPage);
-  }
-
-  void navigationPage() {
-    switch (_switchCode) {
-      case 'not_logged_in':
-        {
-          Navigator.of(context)
-              .push(MaterialPageRoute(builder: (context) => LoginPage()));
-        }
-        break;
-      case 'not_verified':{
-        Navigator.of(context)
-            .push(MaterialPageRoute(builder: (context) => LoginPage()));
-      }
-      break;
-      case 'Student':
-        {
-          print("user is student");
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => StudentBar()),
-          );
-        }
-        break;
-      case 'Mess InCharge':
-        {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => MessBar()),
-          );
-          print('mess');
-        }
-        break;
-      case 'Hostel InCharge':
-        {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => HostelBar()),
-          );
-          print("hostel");
-        }
-        break;
-      case 'Laundry InCharge':
-        {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => LaundryBar()),
-          );
-          print("Laundry");
-        }
-        break;
-      case 'Security InCharge':
-        {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => SecGuardBar()),
-          );
-          print("Laundry");
-        }
-        break;
-    }
-  }
-
-  void _roleCheck(String userID) {
-    print("inside role check");
-    FirebaseFirestore.instance
-        .collection('users')
-        .doc(userID)
-        .get()
-        .then((value) {
-      if (value.exists) {
-        _switchCode = value.data()['role'];
-        print("$_switchCode");
-      } else {
-        print("ERROR");
-      }
-    });
-  }
-
-
-  void initState() {
-    print("inside init");
-    super.initState();
-    startTimer();
-    var _user = FirebaseAuth.instance.currentUser;
-    if (_user != null) {
-      _userID = _user.uid;
-      if (_user.emailVerified == true){
-        print("user id : $_userID");
-      _roleCheck(_userID);
-    }else{
-        _switchCode='not_verified';
-        _user.sendEmailVerification();
-      }
-
-    } else {
-      _switchCode = 'not_logged_in';
-    }
-  }
-//  void initState() {
-//    print("inside init");
-//    super.initState();
-//
-//    var _user = FirebaseAuth.instance.currentUser;
-//    if (_user != null) {
-//      if (!_user.emailVerified) {
-//        setState(() {
-//          Navigator.push(
-//            context,
-//            MaterialPageRoute(builder: (context) => LoginPage()),
-//          );
-//        });
-//      } else {
-//        setState(() {
-//          _userID = _user.uid;
-//          print("user id : $_userID");
-//          _roleCheck(_userID);
-//        });
-//        //Verified
-//      }
-//    } else {
-//      _userID = _user.uid;
-//      print("user id : $_userID");
-//      _roleCheck(_userID);
-//      _switchCode = 'not_logged_in';
-//    }
-//  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
