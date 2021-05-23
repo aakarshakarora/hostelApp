cebb9c6e0d81ad9f9585e53d8155631e47725e89
Aditya Singhai <adityasinghai11.as@gmail.com>
2021-05-23 22:54:02 +0530

    Added the chip widget for the foodlist

--- lib/function/mess/addFoodItem.dart
+++ lib/function/mess/addFoodItem.dart
@@ -1,55 +1,59 @@
 import 'package:flutter/material.dart';
 
-class FoodItemList extends StatelessWidget{
-  final String item1, item2, item3;
-  const FoodItemList(this.item1, this.item2, this.item3); 
-  @override 
-  Widget build(BuildContext context){
-    return Container(
-      child: Row(    
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          OutlinedButton(
-            onPressed: (){}, 
-            child: Text(item1),
-            style: OutlinedButton.styleFrom(
-              onSurface: Colors.black,
-              primary: Colors.black,
-              minimumSize: Size(100,100),
-              shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(2)),
+import '../../theme/theme.dart';
+
+class FoodItemList extends StatelessWidget {
+  final List<String> foodItems;
+  const FoodItemList({this.foodItems});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: RawScrollbar(
+        radius: Radius.circular(5),
+        thumbColor: darkBlue,
+        thickness: 5,
+        child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2),
+          physics: AlwaysScrollableScrollPhysics(),
+          shrinkWrap: true,
+          itemCount: foodItems.length,
+          scrollDirection: Axis.vertical,
+          itemBuilder: (ctx, index) {
+            // return Padding(
+            //   padding: const EdgeInsets.all(8.0),
+            //   child: Container(
+            //     color: Colors.blue,
+            //     height: 10,
+            //     //width: 10,
+            //   ),
+            // );
+            return Padding(
+              padding: const EdgeInsets.all(3.0),
+              child: InkWell(
+                onTap: () {
+                  print(foodItems[index]);
+                },
+                child: InputChip(
+                  backgroundColor: Colors.orange,
+                  selectedColor: Colors.blue,
+                  elevation: 5,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10))
+                  ),
+                  label: Text(foodItems[index],style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 17,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Poppins'
+                  ),),
+                ),
               ),
-            ),
-          ),
-          SizedBox(width: 5.0,),
-          OutlinedButton(
-            onPressed: (){}, 
-            child: Text(item2),
-            style: OutlinedButton.styleFrom(
-              primary: Colors.black,
-              minimumSize: Size(100,100),
-              shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(2)),
-              ),
-            ),
-          ),
-          SizedBox(width: 5.0,),
-          OutlinedButton(
-            onPressed: (){}, 
-            child: Text(item3),
-            style: OutlinedButton.styleFrom(
-              onSurface: Colors.black,
-              primary: Colors.black,
-              minimumSize: Size(100,100),
-              shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(2)),
-              ),
-            ),
-          ),
-        ],
+            );
+          },
+        ),
       ),
-    );   
-  } 
+    );
+  }
 }
