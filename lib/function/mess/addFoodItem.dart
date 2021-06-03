bd41b6a2e48f000d7fea5b7313f2a3a7486c6e57
Aditya Singhai <adityasinghai11.as@gmail.com>
2021-06-03 13:59:27 +0530

    Added the chip widget for the foodlist along with various functions

--- lib/function/mess/addFoodItem.dart
+++ lib/function/mess/addFoodItem.dart
@@ -2,10 +2,22 @@ import 'package:flutter/material.dart';
 
 import '../../theme/theme.dart';
 
-class FoodItemList extends StatelessWidget {
-  final List<String> foodItems;
-  const FoodItemList({this.foodItems});
+class FoodItemList extends StatefulWidget {
+  final List<String> foodItems,foodItemsSelected;
+  const FoodItemList({this.foodItems,this.foodItemsSelected});
 
+  @override
+  _FoodItemListState createState() => _FoodItemListState();
+}
+
+class _FoodItemListState extends State<FoodItemList> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+  }
   @override
   Widget build(BuildContext context) {
     return Expanded(
@@ -15,10 +27,10 @@ class FoodItemList extends StatelessWidget {
         thickness: 5,
         child: GridView.builder(
           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2),
+              crossAxisCount: 3),
           physics: AlwaysScrollableScrollPhysics(),
           shrinkWrap: true,
-          itemCount: foodItems.length,
+          itemCount: widget.foodItems.length,
           scrollDirection: Axis.vertical,
           itemBuilder: (ctx, index) {
             // return Padding(
@@ -30,24 +42,54 @@ class FoodItemList extends StatelessWidget {
             //   ),
             // );
             return Padding(
-              padding: const EdgeInsets.all(3.0),
+              padding: const EdgeInsets.all(2.0),
               child: InkWell(
                 onTap: () {
-                  print(foodItems[index]);
+                  print(widget.foodItems[index]);
                 },
-                child: InputChip(
-                  backgroundColor: Colors.orange,
-                  selectedColor: Colors.blue,
-                  elevation: 5,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10))
+                child: FilterChip(
+                  padding: EdgeInsets.all(3.5),
+                  avatar: CircleAvatar(
+                    backgroundColor: peach,
+                    child: Text(
+                      widget.foodItems[index][0].toUpperCase(),
+                      style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontSize: 17.0,
+                        color: Colors.white,
+                      ),
+                    ),
                   ),
-                  label: Text(foodItems[index],style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 17,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'Poppins'
-                  ),),
+                  elevation: 5,
+                  shadowColor: Colors.deepOrange,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20))
+                  ),
+                  label: Text(
+                    widget.foodItems[index],
+                    // foodItems[index].toString(),
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Poppins',
+                    ),
+                  ),
+                  backgroundColor: Colors.blue[500],
+                  selectedColor: Colors.orange,
+                  selected: widget.foodItemsSelected.contains(widget.foodItems[index]),
+                  onSelected: (bool selected){
+                    setState(() {
+                      if(selected){
+                        widget.foodItemsSelected.add(widget.foodItems[index]);
+                      }
+                      else {
+                        widget.foodItemsSelected.removeWhere((String name){
+                          return name == widget.foodItems[index];
+                        });
+                      }
+                    });
+                  },
                 ),
               ),
             );
--- lib/function/mess/addMenu.dart
+++ lib/function/mess/addMenu.dart
@@ -1,22 +1,127 @@
+import 'dart:ffi';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';               
+import 'package:flutter/material.dart';
 import 'package:hostel_app/function/mess/addFoodItem.dart';
 import 'package:hostel_app/theme/theme.dart';
 
 //Status: Working fine
 class AddMenuItem extends StatefulWidget {
+  String mealOfDay;
 
-  String mealOfDay;            
-  AddMenuItem({this.mealOfDay});         
+  AddMenuItem({this.mealOfDay});
 
   @override
   _AddMenuItemState createState() => _AddMenuItemState();
-}                                                                 
+}
 
 class _AddMenuItemState extends State<AddMenuItem> {
   final messController = TextEditingController();
-  @override           
+  List<String> _brekfastSelected;
+  List<String> _lunchSelected;
+  List<String> _hiTeaSelected;
+  List<String> _dinnerSelected;
+  List<String> breakfastSuggestion = [
+    "Tea",
+    "Coffee",
+    "Hot Milk",
+    "Pav Bhaji",
+    "Dosa",
+    "Idli",
+    "Vada",
+    "Utpam",
+    "Upma",
+    "Sambhar",
+    "Poha",
+    "Pasta",
+    "Noodles",
+    "Allo Prantha",
+    "Curd",
+    "Tomato Chutney",
+    "Chole Bhature",
+    "Poori Bhaji",
+    "Bread",
+    "Jam",
+    "Butter",
+    "Fruits",
+    "Egg"
+  ];
+  List<String> lunchSuggestion = [
+    "Rice",
+    "Chapati",
+    "Kofta",
+    "Papad",
+    "Fryums",
+    "Lassi",
+    "Pickle",
+    "Green Salad",
+    "Aloo Ki Sabzi",
+    "Bhindi do Pyaaz",
+    "Rajma",
+    "Kadi Pakoda",
+    "Pindi Chole",
+    "Black Chana",
+    "Chawli Masala",
+    "Dal Fry",
+    "Toor Dal",
+    "Dal Mysore",
+    "Soya Chap",
+    "Mix Veg"
+  ];
+  List<String> hiTeaSuggestion = [
+    "Tea",
+    "Coffee",
+    "Samosa",
+    "Allo Onion Kachori",
+    "Paani-Puri",
+    "Pizza",
+    "Pasta",
+    "Noodles",
+    "Veg Puff",
+    "Bhel",
+    "Dhokha",
+    "Aloo Bhonda",
+    "Vada Pav",
+    "Fried Idli",
+    "Daal Kachori",
+    "Doughnut",
+    "Allo Tikki",
+    "Maggi",
+    "Potato Veg",
+    "Veg Sandwich",
+    "Mix Veg Pakoda"
+  ];
+  List<String> dinnerSuggestion = [
+    "Kadai paneer",
+    "Paneer",
+    "Malai Kofta",
+    "Rajma",
+    "Chapati",
+    "Custard",
+    "Gulab Jamun",
+    "Shahi Bread",
+    "Fried Rice"
+  ];
+
+  // List<DocumentSnapshot> getBreakfastData(){
+  //   FirebaseFirestore.instance.collection('messMenu').get().then((querySnapshot) {
+  //     querySnapshot.docs.forEach((result) {
+  //       return result.data()["Breakfast"]["Monday"];
+  //     });
+  //   });
+  // }
+  @override
+  void initState() {
+    // TODO: implement initState
+    _brekfastSelected = <String>[];
+    _lunchSelected = <String>[];
+    _hiTeaSelected = <String>[];
+    _dinnerSelected = <String>[];
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     String newFoodTitle;
     return Container(
@@ -25,63 +130,81 @@ class _AddMenuItemState extends State<AddMenuItem> {
         padding: EdgeInsets.all(10),
         decoration: BoxDecoration(
             color: Colors.white,
-            borderRadius: BorderRadius.only(topLeft: Radius.circular(30), topRight: Radius.circular(30))
-        ),
-        child: Column(    
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30), topRight: Radius.circular(30))),
+        child: Column(
           children: [
-            Text("Add Food Item",
-              style: darkHeading,),
-            SizedBox(height: 20,),
+            Text(
+              "Add Food Item",
+              style: darkHeading,
+            ),
+            SizedBox(
+              height: 20,
+            ),
 
             // TextFormField --------------------------
             TextFormField(
               controller: messController,
-              decoration: kTextFieldDecoration.copyWith(labelText: 'Enter Food Item'),
+              decoration:
+                  kTextFieldDecoration.copyWith(labelText: 'Enter Food Item'),
               // ignore: missing_return
               validator: (String value) {
                 if (value.isEmpty) {
-                  return 'Field is required';       
+                  return 'Field is required';
                 }
               },
               onSaved: (String value) {
                 widget.mealOfDay = value;
               },
             ),
-            SizedBox(height: 10.0,),
 
+            // SizedBox(height: 5.0,),
             // -----------------------------------
 
             // ----- Something New ---------------
-            FoodItemList(foodItems: ['Item One', 'Item Two','Item Three'],),
-            // SizedBox(height: 20.0,),
-            // FoodItemList('Item Four', 'Item Five', 'Item Six'),
-            // -----------------------------------
-            SizedBox(height: 10,),
+            FoodItemList(
+                foodItems: widget.mealOfDay == 'breakfast'
+                    ? breakfastSuggestion
+                    : widget.mealOfDay == 'lunch'
+                        ? lunchSuggestion
+                        : widget.mealOfDay == 'hiTea'
+                            ? hiTeaSuggestion
+                            : dinnerSuggestion,
+            foodItemsSelected:widget.mealOfDay == 'breakfast'
+                ? _brekfastSelected
+                : widget.mealOfDay == 'lunch'
+                ? _lunchSelected
+                : widget.mealOfDay == 'hiTea'
+                ? _hiTeaSelected
+                : _dinnerSelected, ),
+            SizedBox(
+              height: 10,
+            ),
             Container(
-              decoration: kloginScreenButtonStyle.copyWith(),
-              child: TextButton(
-                style: TextButton.styleFrom(
-                    padding: EdgeInsets.symmetric(vertical: 0,horizontal: 0.06*MediaQuery.of(context).size.width),
-                    alignment: Alignment.center),
-                child: Text("Add",
-                  style: kbuttonTextStyle,),
+              decoration: kloginScreenButtonStyle.copyWith(color: peach),
+              child: FlatButton(
+                child: Text(
+                  "Add",
+                  style: kbuttonTextStyle,
+                ),
                 onPressed: () async {
                   print(newFoodTitle);
-                  DocumentReference docRef = FirebaseFirestore.instance.collection('messMenu').doc('EzkkLnYtQPbSDLxMToZm');
+                  DocumentReference docRef = FirebaseFirestore.instance
+                      .collection('messMenu')
+                      .doc('EzkkLnYtQPbSDLxMToZm');
                   DocumentSnapshot doc = await docRef.get();
-                  docRef.update(
-                      {widget.mealOfDay :FieldValue.arrayUnion([messController.text])}
-                  );
+                  docRef.update({
+                    widget.mealOfDay:
+                        FieldValue.arrayUnion([messController.text])
+                  });
 
                   Navigator.pop(context);
                 },
               ),
-            ),
-
+            )
           ],
         ),
       ),
     );
   }
 }
-
